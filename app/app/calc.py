24720aae1e1b5ba8ef44e8b41dfb59c96dec13dc
dudekzwp <dudekzwp@gmail.com>
2023-04-14 21:21:14 +0200

    Init unittests

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator ffunctions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtracts x from y and return result"""
+    return y - x
